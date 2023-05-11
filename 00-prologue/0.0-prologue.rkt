35fba10676eb01ea1e125d2a137963c93df83ef7
alexdesiqueira <alex.desiqueira@igdore.org>
2023-05-11 10:43:25 -0300

    Add Prologue address, small changes in structure

--- 00-prologue/0.0-prologue.rkt
+++ 00-prologue/0.0-prologue.rkt
@@ -1,6 +1,9 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname |0|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname 0.0-prologue) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; Prologue: How to Program
+;; https://htdp.org/2023-3-6/Book/part_prologue.html
+
 (+ 1 1) ;; click "Run"
 ;; 2
 
@@ -15,40 +18,35 @@
 
 ;; the expressions above and the following ones are then typed in the
 ;; _interactions area_:
-;;
-;; > (sqr 3)
+(sqr 3)
 ;; 9
-;; > (expt 2 3)
+(expt 2 3)
 ;; 8
-;; > (sin 0)
+(sin 0)
 ;; 0
-;; > (cos pi)
+(cos pi)
 ;; #i-1.0  ;; #i is an _inexact number_
 
 ;; adding more than two numbers at once
-;;
-;; > (+ 2 (+ 3 4))  ;; nested arithmetic
+(+ 2 (+ 3 4))  ;; nested arithmetic
 ;; 9
-;; > (+ 2 3 4)  ;; BSL arithmetic
+(+ 2 3 4)  ;; BSL arithmetic
 ;; 9
 
 ;; nesting examples
-;;
-;;> (+ 2 (+ (* 3 3) 4))
+(+ 2 (+ (* 3 3) 4))
 ;; 15
-;; > (+ 2 (+ (* 3(/ 12 4)) 4))
+(+ 2 (+ (* 3(/ 12 4)) 4))
 ;; 15
-;; > (+ (* 5 5) (+ (* 3 (/ 12 4)) 4))
+(+ (* 5 5) (+ (* 3 (/ 12 4)) 4))
 ;; 38
 
 ;; mind the extra parentheses
-;;
-;; > (+ (1) (2))
+; (+ (1) (2))
 ;; function call: expected a function after the open parenthesis, but found a number
 
 ;; operations on several operands at once
-;;
-;; > (+ 1 2 3 4 5 6 7 8 9 0)
+(+ 1 2 3 4 5 6 7 8 9 0)
 ;; 45
-;; > (* 1 2 3 4 5 6 7 8 9 0)
+(* 1 2 3 4 5 6 7 8 9 0)
 ;; 0
